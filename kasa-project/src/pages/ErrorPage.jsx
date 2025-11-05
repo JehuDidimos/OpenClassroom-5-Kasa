454b3b5d059323079b6a62acaba944a6a666661e
JehuDidimos <jehudidimos.cs@gmail.com>
2025-11-05 14:34:48 -0600

    Error styling

--- kasa-project/src/pages/ErrorPage.jsx
+++ kasa-project/src/pages/ErrorPage.jsx
@@ -1,12 +1,25 @@
 import FooterComponent from "../components/FooterComponent";
 import HeaderComponent from "../components/HeaderComponent";
+import "../styles/pages/ErrorPageStyles.scss";
 
 function ErrorPage() {
     return (
       <div className="main-container">
         <>
           <div className="main-container">
-            Error Page
+            <div className="error-code">
+              404
+            </div>
+
+            <div className="error-message">
+              Oops! The page you’re looking for doesn’t exist.
+            </div>
+
+            <div className="home-link">
+              <a href="/home">
+                Return to home page
+              </a>
+            </div>
           </div>
         </>
       </div>
