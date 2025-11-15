00fbefd6f91f0ea92b2392d8788d302a2a38681d
JehuDidimos <91406011+JehuDidimos@users.noreply.github.com>
2025-11-15 12:15:17 -0600

    Sass architecture

--- kasa-project/src/components/DropdownComponent.jsx
+++ kasa-project/src/components/DropdownComponent.jsx
@@ -8,19 +8,20 @@ function DropdownComponent() {
         setFlipped(prev => !prev);
         console.log(flipped);
     }
-    return ( 
-        <div className="dropdown-main" onClick={() => toggleFlipAnimation()} >
-            <div className="dropdown-title">
-                Sample Title
-            </div>
-            <div className="vector">
-                <img 
-                className={flipped ? "vector-normal" : "vector-flipped"}
-                src={closedVector} 
-                />
-            </div>
+    return (
+      <>
+        <div className="dropdown-main" onClick={() => toggleFlipAnimation()}>
+          <div className="dropdown-title">Sample Title</div>
+          <div className="vector">
+            <img
+              className={flipped ? "vector-normal" : "vector-flipped"}
+              src={closedVector}
+            />
+          </div>
         </div>
-     );
+        <div className={flipped ? "content-shown" : "content-hidden"}></div>
+      </>
+    );
 }
 
 export default DropdownComponent;
